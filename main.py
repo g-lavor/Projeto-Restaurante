683152a6e1fff07adf24ae2440397e112fb000ec
Gabriel Lavôr <103610943+g-lavor@users.noreply.github.com>
2023-12-16 10:40:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import csv
 import pandas as pd
-from functools import reduce
 from abc import ABC, abstractmethod
 
 class Restaurante:
@@ -30,6 +29,7 @@ class Restaurante:
 
 class Requisitos(ABC):
     def __init__(self, valor):
+        # Classe abstrata que define a estrutura para os requisitos de pesquisa
         self.__valor = valor
 
     @property
@@ -41,106 +41,96 @@ class Requisitos(ABC):
 
 class NomeRequisitos(Requisitos):
     def verificar(self, restaurante):
-        return self.valor.lower() in restaurante.nome.lower()
+        valor = self.valor.lower().replace(" ", "")  # Remover espaços e tornar minúsculo
+        restaurante_nome = restaurante.nome.lower().replace(" ", "")  # Remover espaços e tornar minúsculo
+
+        # Sempre realizar pesquisa por prefixo
+        prefixo = valor
+        return restaurante_nome.startswith(prefixo)
 
 class CulinariaRequisitos(Requisitos):
     def verificar(self, restaurante):
+        # Verifica se o tipo de culinária do restaurante atende aos critérios
         return self.valor.lower() == restaurante.culinaria.lower()
 
 class ClassificacaoRequisitos(Requisitos):
     def verificar(self, restaurante):
+        # Verifica se a classificação do restaurante atende aos critérios
         return restaurante.classificacao >= int(self.valor)
 
 class DistanciaRequisitos(Requisitos):
     def verificar(self, restaurante):
+        # Verifica se a distância do restaurante atende aos critérios
         return restaurante.distancia <= int(self.valor)
 
 class PrecoRequisitos(Requisitos):
     def verificar(self, restaurante):
+        # Verifica se o preço do restaurante atende aos critérios
         return restaurante.preco <= float(self.valor)
 
 class PesquisaRestaurante:
     def __init__(self, restaurantes):
+        # Classe que realiza pesquisas em uma lista de restaurantes
         self.__restaurantes = restaurantes
 
     def realizar_pesquisa(self, *requisitos):
-        resultados = self.__restaurantes[:]
+        # Realiza a pesquisa considerando os requisitos fornecidos
+        resultados = set(self.__restaurantes)
         for requisito in requisitos:
-            resultados = [restaurante for restaurante in resultados if requisito.verificar(restaurante)]
-        resultados.sort(key=lambda r: (r.distancia, -r.classificacao, r.preco))
+            resultados = set(filter(requisito.verificar, resultados))
+        resultados = sorted(resultados, key=lambda r: (r.distancia, -r.classificacao, r.preco))
         return resultados[:5]
 
 def carregar_dados_csv(arquivo_restaurantes, arquivo_culinarias):
+    # Carrega dados de restaurantes e culinárias a partir de arquivos CSV
     with open(arquivo_restaurantes, newline='', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         dados = [dict(row) for row in reader]
-
-    # Carregar os dados do arquivo de culinária em um DataFrame do pandas
     df_culinaria = pd.read_csv(arquivo_culinarias)
-
-    # Criar um dicionário a partir do DataFrame para mapear 'id' para 'name'
     mapeamento_culinaria = df_culinaria.set_index('id')['name'].to_dict()
-
-    # Substituir os IDs de culinária pelos nomes correspondentes usando o dicionário de mapeamento
     for restaurante in dados:
-        # Trocar o cuisine_id pelo nome correspondente usando o dicionário
         restaurante['cuisine_id'] = mapeamento_culinaria.get(int(restaurante['cuisine_id']), restaurante['cuisine_id'])
     return [Restaurante(**restaurante) for restaurante in dados]
 
-
-def find_intersection(lists):
-    if not lists:
-        return []
-    restaurant_sets = [set(stream) for stream in lists]
-    intersection = reduce(lambda set1, set2: set1.intersection(set2), restaurant_sets)
-    return list(intersection)
-
 def realizar_pesquisa_com_usuario():
-    # Carregar dados do arquivo CSV
+    # Realiza uma pesquisa com base nas escolhas do usuário
     restaurantes = carregar_dados_csv('restaurants.csv', 'cuisines.csv')
-
-    # Criar instância da classe de Pesquisa
     pesquisa = PesquisaRestaurante(restaurantes)
+    print('Como deseja realizar a pesquisa?\n1 - Nome\n2 - Culinaria\n3 - Distancia\n4 - Classificação\n5 - Preço')
+    escolhas = input('Digite os números das opções desejadas, separados por vírgula (ex: 1,3,5): ')
+    escolhas = [int(e) for e in escolhas.split(',') if e.isdigit() and 1 <= int(e) <= 5]
+    resultados_intermediarios = []
 
-    
-    # Perguntar ao usuário como eles querem pesquisar
-    print('Como deseja realizar a pesquisa ?\n1 - Nome\n2 - Culinaria\n3 - Distancia\n4 - Classificação\n5 - Preço\n6 - Tudo')
-    escolha = int(input())
+    for escolha in escolhas:
+        if escolha == 1:
+            nome = input('Qual o nome do restaurante? ')
+            resultados_intermediarios.append(NomeRequisitos(nome))
+        elif escolha == 2:
+            culinaria = input('Qual o tipo de culinária? ')
+            resultados_intermediarios.append(CulinariaRequisitos(culinaria))
+        elif escolha == 3:
+            distancia = int(input('Qual a distância máxima (em km)? '))
+            resultados_intermediarios.append(DistanciaRequisitos(distancia))
+        elif escolha == 4:
+            classificacao = int(input('Qual a classificação mínima (de 1 a 5)? '))
+            resultados_intermediarios.append(ClassificacaoRequisitos(classificacao))
+        elif escolha == 5:
+            preco = float(input('Qual o preço máximo (em reais)? '))
+            resultados_intermediarios.append(PrecoRequisitos(preco))
+    resultado_pesquisa = pesquisa.realizar_pesquisa(*resultados_intermediarios)
 
-    # Dependendo da escolha do usuário, perguntar o valor específico para o critério e realizar a pesquisa apropriada
-    if escolha == 1:
-        nome = input('Qual o nome do restaurante? ')
-        resultado_pesquisa = pesquisa.realizar_pesquisa(NomeRequisitos(nome))
-    elif escolha == 2:
-        culinaria = input('Qual o tipo de culinária? ')
-        resultado_pesquisa = pesquisa.realizar_pesquisa(CulinariaRequisitos(culinaria))
-    elif escolha == 3:
-        distancia = int(input('Qual a distância máxima (em km)? '))
-        resultado_pesquisa = pesquisa.realizar_pesquisa(DistanciaRequisitos(distancia))
-    elif escolha == 4:
-        classificacao = int(input('Qual a classificação mínima (de 1 a 5)? '))
-        resultado_pesquisa = pesquisa.realizar_pesquisa(ClassificacaoRequisitos(classificacao))
-    elif escolha == 5:
-        preco = float(input('Qual o preço máximo (em reais)? '))
-        resultado_pesquisa = pesquisa.realizar_pesquisa(PrecoRequisitos(preco))
-    elif escolha == 6:
-        nome = input('Qual o nome do restaurante? ')
-        culinaria = input('Qual o tipo de culinária? ')
-        distancia = int(input('Qual a distância máxima (em km)? '))
-        classificacao = int(input('Qual a classificação mínima (de 1 a 5)? '))
-        preco = float(input('Qual o preço máximo (em reais)? '))
-        resultado_nome = pesquisa.realizar_pesquisa(NomeRequisitos(nome))
-        resultado_culinaria = pesquisa.realizar_pesquisa(CulinariaRequisitos(culinaria))
-        resultado_distancia = pesquisa.realizar_pesquisa(DistanciaRequisitos(distancia))
-        resultado_classificacao = pesquisa.realizar_pesquisa(ClassificacaoRequisitos(classificacao))
-        resultado_preco = pesquisa.realizar_pesquisa(PrecoRequisitos(preco))
-        resultado_pesquisa = find_intersection([resultado_nome, resultado_culinaria, resultado_distancia, resultado_classificacao, resultado_preco])
+    if resultado_pesquisa:
+        print("Resultados da Pesquisa:\n")
+        for restaurante in resultado_pesquisa:
+            print(
+                'Nome:', restaurante.nome, '\nDistância:', restaurante.distancia,
+                '\nClassificação:', restaurante.classificacao, '\nPreço:', restaurante.preco,
+                '\nCulinaria:', restaurante.culinaria
+            )
+            print()
+            print()
+    else:
+        print("Sem restaurantes disponíveis")
 
-# Exibir resultados da pesquisa
-    print("Resultados da Pesquisa:")
-    for restaurante in resultado_pesquisa:
-        print('Nome:',restaurante.nome, '\nDistância:',restaurante.distancia, '\nClassificação:',restaurante.classificacao, '\nPreço:',restaurante.preco, '\nCulinaria:',restaurante.culinaria)
-        print()
-        print()
-
+# Chama a função para realizar a pesquisa com o usuário
 realizar_pesquisa_com_usuario()
